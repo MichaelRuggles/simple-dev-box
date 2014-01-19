af0fbbbe80b973c79fa833e4cdab68b882c8c4fc
Michael Ruggles <ruggl015@umn.edu>
2014-01-19 11:41:37 -0600

    Add 6 test functions.

--- karma-requirejs/test/app_spec.js
+++ karma-requirejs/test/app_spec.js
@@ -16,7 +16,31 @@ define(['app', 'jquery', 'lodash'], function(App, $, _) {
 		it('true equals true', function(){
 			expect( true ).toEqual(true);
 		});
-		
+
+		it('4*3 equals 12', function(){
+			expect(4*3).toEqual(12);
+		});
+
+		it('30/5 equals 6', function(){
+			expect(30/5).toEqual(6);
+		});
+
+		it('Even mod 2 equals 0', function(){
+			expect(16%2).toEqual(0);
+		});
+
+		it('10>5 equals true', function(){
+			expect(10>5).toEqual(true);
+		});
+
+		it('negative number^2 greater than zero', function(){
+			expect(((-2)*(-2)) > 0).toEqual(true);
+		});
+
+		it('30-37 equals -7', function(){
+			expect(30-37).toEqual(-7);
+		});
+
 	});
 
 });
